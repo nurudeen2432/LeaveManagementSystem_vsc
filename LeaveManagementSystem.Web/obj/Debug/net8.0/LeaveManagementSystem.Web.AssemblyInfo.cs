9e5ff27ae8337df1a6804b967c68e45eba926251
Nurudeen Durowade <Nurudeen.durowade@wemabank.com>
2024-12-07 00:14:31 +0100

    new changes

--- LeaveManagementSystem.Web/obj/Debug/net8.0/LeaveManagementSystem.Web.AssemblyInfo.cs
+++ LeaveManagementSystem.Web/obj/Debug/net8.0/LeaveManagementSystem.Web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeaveManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f58348fe8114dd039df30583fc7de7960b5bd76")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeaveManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeaveManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
