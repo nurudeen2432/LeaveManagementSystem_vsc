28897223e5e2ebac685334f9cba31c6125daa496
Nurudeen Durowade <Nurudeen.durowade@wemabank.com>
2025-01-11 00:17:58 +0100

    Add more projets for clean architecture

--- LeaveManagementSystem.Data/obj/Debug/net8.0/LeaveManagementSystem.Data.AssemblyInfo.cs
+++ LeaveManagementSystem.Data/obj/Debug/net8.0/LeaveManagementSystem.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeaveManagementSystem.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d1ccea08d216754bbfca807dc93118f17799cf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65f969c0e2f62ff3b839aabda96d934df5e0c0d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeaveManagementSystem.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeaveManagementSystem.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
